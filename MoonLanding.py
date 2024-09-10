9cc9c2bef44bcca99a6ff6e48d054548e917e7d9
MoonMace <Mace.seffian@gmail.com>
2024-09-10 18:06:05 -0400

    Add files via upload
    I did some initial building of the program. You may have to install pygame, in which case, you can use the anaconda prompt and type:
    >pip install pygame

---
+++ MoonLanding.py
@@ -0,0 +1,61 @@
+import pygame as pg
+import numpy as np
+import time as t
+
+
+# Initialize pygame
+pg.init()
+
+# Create the height and width of the window that the game runs in
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 1000
+
+
+pos = np.array([475,475])
+
+# Create the ship
+ship = pg.Rect((pos[0],pos[1],50,50))
+
+# Create the screen
+screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pg.display.set_caption('Moon Lander')
+
+
+# load images
+# Background
+# background_img = pg.image.load('').convert_alpha    # This will be where we load whatever background image of the moon we get
+
+
+
+
+
+
+run = True
+while run:
+    
+    screen.fill((0,0,0))
+    
+    # Place the ship
+    pg.draw.rect(screen, (50,50,50), ship)
+    
+    key = pg.key.get_pressed()
+    if key[pg.K_s] == True:
+        ship.move_ip(0,-1)
+    elif key[pg.K_w] == True:
+        ship.move_ip(0,1)
+    elif key[pg.K_a] == True:
+        ship.move_ip(1,0)
+    elif key[pg.K_d] == True:
+        ship.move_ip(-1,0)
+    
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            run = False
+            
+            
+           
+        
+
+    t.sleep(0.003)     
+    pg.display.update()
+pg.quit()
