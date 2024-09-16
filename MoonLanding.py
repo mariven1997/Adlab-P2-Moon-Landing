f3b16bd270b622b4b694000cd1b55000875cc595
KamrynMcGary <mcgaryit@dukes.jmu.edu>
2024-09-16 15:59:52 -0400

    Update MoonLanding.py
    Implemented loops for timstepping properly. Ish

--- MoonLanding.py
+++ MoonLanding.py
@@ -73,26 +73,38 @@ while run:
     
     key = pg.key.get_pressed()
     if key[pg.K_s] == True:
-        #S key should rotate the engine to face DOWN
-        shiprect.move_ip(MotionX(Vx, 0, TimeStep, Mass, -1*BurnRate), MotionY(Vy, Thrust, TimeStep, Mass, -1*BurnRate))
-        Mass += -1*BurnRate
-        Vy = Vy + Thrust*TimeStep - g*TimeStep
+        LoopBuddy = 0
+        while LoopBuddy <= TurnLength:        
+            #S key should rotate the engine to face DOWN
+            shiprect.move_ip(MotionX(Vx, 0, TimeStep, Mass, -1*BurnRate), MotionY(Vy, Thrust, TimeStep, Mass, -1*BurnRate))
+            Mass += -1*BurnRate
+            Vy = Vy + Thrust*TimeStep - g*TimeStep
+            LoopBuddy += TimeStep
     if key[pg.K_a] == True:
-        #A key should rotate the engine to face RIGHT
-        shiprect.move_ip((MotionX(Vx, -1*Thrust, TimeStep, Mass, -1*BurnRate), MotionY(Vy, 0, TimeStep, Mass, -1*BurnRate)))
-        Mass += -1*BurnRate
-        Vx = Vx - Thrust*TimeStep
-        Vy = Vy - g*TimeStep
+        LoopBuddy = 0
+        while LoopBuddy <= TurnLength:
+#A key should rotate the engine to face RIGHT
+            shiprect.move_ip((MotionX(Vx, -1*Thrust, TimeStep, Mass, -1*BurnRate), MotionY(Vy, 0, TimeStep, Mass, -1*BurnRate)))
+            Mass += -1*BurnRate
+            Vx = Vx - Thrust*TimeStep
+            Vy = Vy - g*TimeStep
+            LoopBuddy += TimeStep
     if key[pg.K_d] == True:
-        #D key should rotate the engine to face LEFT
-        shiprect.move_ip((MotionX(Vx, Thrust, TimeStep, Mass, -1*BurnRate), MotionY(Vy, 0, TimeStep, Mass, -1*BurnRate)))
-        Mass += -1*BurnRate
-        Vx = Vx + Thrust*TimeStep
-        Vy = Vy - g*TimeStep
+        LoopBuddy = 0
+        while LoopBuddy <= TurnLength:
+            #D key should rotate the engine to face LEFT
+            shiprect.move_ip((MotionX(Vx, Thrust, TimeStep, Mass, -1*BurnRate), MotionY(Vy, 0, TimeStep, Mass, -1*BurnRate)))
+            Mass += -1*BurnRate
+            Vx = Vx + Thrust*TimeStep
+            Vy = Vy - g*TimeStep
+            LoopBuddy += TimeStep
     if key[pg.K_w] == True:
-        # W key should wait
-        shiprect.move_ip(MotionX(Vx, 0, TimeStep, Mass, -1*BurnRate), MotionY(Vy, 0, TimeStep, Mass, 0))
-        Vy = Vy - g*TimeStep
+        LoopBuddy = 0
+        while LoopBuddy <= TurnLength:
+            # W key should wait
+            shiprect.move_ip(MotionX(Vx, 0, TimeStep, Mass, -1*BurnRate), MotionY(Vy, 0, TimeStep, Mass, 0))
+            Vy = Vy - g*TimeStep
+            LoopBuddy += TimeStep
     
     
     for event in pg.event.get():
